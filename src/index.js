3ca42572bfd580748422b9e3fd814fa94796ddab
Luke Edwards <luke.edwards05@gmail.com>
2019-01-26 12:30:52 -0800

    major: use (mod 6) approach

--- src/index.js
+++ src/index.js
@@ -1,23 +1,12 @@
 export default function (num) {
-	if (num < 2 || num * 0 !== 0) {
-		return false;
-	}
+	if (num * 0 !== 0) return false;
 
-	var i=0, j, sqrt, arr=[2, 3, 5];
+	if (num <= 3) return num > 1;
+	if (num % 2 === 0 || num % 3 === 0) return false;
 
-	for (; i < arr.length; i++) {
-		if (num === arr[i]) return true;
-		if (num % arr[i] === 0) return false;
-	}
-
-	arr = [0, 4, 6, 10, 12, 16, 22, 24];
-	sqrt = Math.sqrt(num);
-
-	for (i=7; i <= sqrt; i += 30) {
-		for (j=0; j < arr.length; j++) {
-			if (num % (i + arr[j]) === 0) {
-				return false;
-			}
+	for (var i = 5; i * i <= num; i += 6) {
+		if (num % i === 0 || num % (i + 2) === 0) {
+			return false
 		}
 	}
 
