f05f913f41377b7acb2b7e7fda176194bd8cdfb2
Luke Edwards <luke.edwards05@gmail.com>
2019-01-26 12:31:17 -0800

    chore: move previous into `bench/arguiot` file

---
+++ bench/arguiot.js
@@ -0,0 +1,29 @@
+// Authored: Arthur Goi (@arguiot)
+// Refactored: Luke Edwards (@lukeed)
+// @see https://github.com/lukeed/primeval/pull/2#issuecomment-457814536
+
+module.exports = function (num) {
+	if (num < 2 || num * 0 !== 0) {
+		return false;
+	}
+
+	var i=0, j, sqrt, arr=[2, 3, 5];
+
+	for (; i < arr.length; i++) {
+		if (num === arr[i]) return true;
+		if (num % arr[i] === 0) return false;
+	}
+
+	arr = [0, 4, 6, 10, 12, 16, 22, 24];
+	sqrt = Math.sqrt(num);
+
+	for (i=7; i <= sqrt; i += 30) {
+		for (j=0; j < arr.length; j++) {
+			if (num % (i + arr[j]) === 0) {
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
