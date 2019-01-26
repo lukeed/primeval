3dc96e7103f777da34c5bc265f7d68ce58988fdb
Luke Edwards <luke.edwards05@gmail.com>
2019-01-26 00:43:03 -0800

    refactor: simplify logic & drop 50 bytes~!

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
-export default function(n) {
-	if (n == 2) return true
-	if (n < 2 || isNaN(n) || !isFinite(n)) return false
-	var out = false
-	if (n == 0) out = out !== false ? out : 0;
-	if (n % 1 || n * n < 2) out = out !== false ? out : 1;
-	if (n % 2 == 0) out = out !== false ? out : 2;
-	if (n % 3 == 0) out = out !== false ? out : 3;
-	if (n % 5 == 0) out = out !== false ? out : 5;
-	var m = Math.sqrt(n);
-	for (let i = 7; i <= m; i += 30) {
-		if (n % i == 0) out = out !== false ? out : i;
-		if (n % (i + 4) == 0) out = out !== false ? out : i + 4;
-		if (n % (i + 6) == 0) out = out !== false ? out : i + 6;
-		if (n % (i + 10) == 0) out = out !== false ? out : i + 10;
-		if (n % (i + 12) == 0) out = out !== false ? out : i + 12;
-		if (n % (i + 16) == 0) out = out !== false ? out : i + 16;
-		if (n % (i + 22) == 0) out = out !== false ? out : i + 22;
-		if (n % (i + 24) == 0) out = out !== false ? out : i + 24;
+export default function (num) {
+	if (num < 2 || num * 0 !== 0) {
+		return false;
 	}
-	out = out !== false ? out : n
-	return n == out && n > 2;
+
+	var i=0, j, sqrt, arr=[2, 3, 5];
+
+	for (; i < arr.length; i++) {
+		if (num === arr[i]) return true;
+		if (num % arr[i] === 0) return false;
+	}
+
+	arr = [0, 4, 6, 10, 12, 16, 22, 24];
+	sqrt = Math.sqrt(num);
+
+	for (i=7; i <= sqrt; i += 30) {
+		for (j=0; j < arr.length; j++) {
+			if (num % (i + arr[j]) === 0) {
+				return false;
+			}
+		}
+	}
+
+	return true;
 }
