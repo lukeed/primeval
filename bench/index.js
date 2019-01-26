4688776710b067aaedb47ef124c262ffc3e99750
Luke Edwards <luke.edwards05@gmail.com>
2019-01-26 01:05:22 -0800

    chore: add benchmark

---
+++ bench/index.js
@@ -0,0 +1,26 @@
+const { Suite } = require('benchmark');
+const primeval = require('../dist/primeval');
+
+const primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199];
+const notprimes = [0, 1, 4, 6, 8, 10, 14, 15, 25, 30, 77, 100, 625, 932];
+const carmichaels = [561, 1105, 1729, 2465, 2821, 6601, 8911, 41041, 825265, 321197185];
+
+const onCycle = e => console.log(String(e.target));
+
+new Suite({ onCycle })
+	.add('(primes) primeval', () => {
+		primes.forEach(primeval);
+	})
+	.run();
+
+new Suite({ onCycle })
+	.add('(notprimes) primeval', () => {
+		notprimes.forEach(primeval);
+	})
+	.run();
+
+new Suite({ onCycle })
+	.add('(carmichaels) primeval', () => {
+		carmichaels.forEach(primeval);
+	})
+	.run();
