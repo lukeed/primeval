419e384235e1c77f2b725c7d63a4e5b66cbbfcf5
Arthur Guiot <arguiot@gmail.com>
2019-01-26 09:57:30 +0800

    Much faster solution

--- src/index.js
+++ src/index.js
@@ -1,35 +1,24 @@
-export default function (num) {
-	if (num < 2) {
-		return false;
-	}
-
-	var i=0, j=0, int=1, arr = new Array(100);
-
-	// If coefs will exceed `Number.MAX_SAFE_INTEGER`
-	//   then we need to use `BigInt` values – Node 10.8+
-	if (num >= 57) {
-		int = BigInt(int);
-		num = BigInt(num);
-	}
-
-	// Coefficients
-	for (arr[0]=int; i < num; i++) {
-		arr[1 + i] = int;
-
-		for (j=i; j > 0; j--) {
-			arr[j] = arr[j - 1] - arr[j];
-		}
-
-		arr[0] = -arr[0]; // safe: BigInt & Number
-	}
-
-	arr[0]++;
-	arr[num]--;
-
-	// Return true if all coefficients
-	//   were divisible by the target number.
-	int -= int; //=> 0 or 0n
-	for (i=num; i-- && arr[i] % num === int;);
-
-	return i < 0;
+export default function (n) {
+  if (n < 2 || isNaN(n) || !isFinite(n)) {
+    return false
+  }
+	var out = false
+  if (n == 0) out = out !== false ? out : 0;
+  if (n % 1 || n * n < 2) out = out !== false ? out : 1;
+  if (n % 2 == 0) out = out !== false ? out : 2;
+  if (n % 3 == 0) out = out !== false ? out : 3;
+  if (n % 5 == 0) out = out !== false ? out : 5;
+  var m = Math.sqrt(n);
+  for (let i = 7; i <= m; i += 30) {
+      if (n % i == 0) out = out !== false ? out : i;
+      if (n % (i + 4) == 0) out = out !== false ? out : i + 4;
+      if (n % (i + 6) == 0) out = out !== false ? out : i + 6;
+      if (n % (i + 10) == 0) out = out !== false ? out : i + 10;
+      if (n % (i + 12) == 0) out = out !== false ? out : i + 12;
+      if (n % (i + 16) == 0) out = out !== false ? out : i + 16;
+      if (n % (i + 22) == 0) out = out !== false ? out : i + 22;
+      if (n % (i + 24) == 0) out = out !== false ? out : i + 24;
+  }
+  out = out !== false ? out : n
+  return n == out && n > 2;
 }
